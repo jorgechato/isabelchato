ac2e4d219c670fa6da662346bf3974b12fad7959
jorgechato <hack@jorgechato.com>
2025-02-12 10:58:43 +0900

    feat(profile): basic profile structure

---
+++ src/config/index.ts
@@ -0,0 +1,28 @@
+import sns from './sns.json';
+import profile from './profile.json';
+import { SOCIAL_MEDIA } from './ui';
+
+const SNS_DICT = { ...sns } as const;
+
+type SNSType = {
+    name: string;
+    icon: any;
+    url: string;
+}
+
+export function getSNS(): SNSType[] {
+    let snsList: SNSType[] = []
+    Object.entries(SNS_DICT).forEach(([key, value]) => {
+        snsList.push({
+            name: SOCIAL_MEDIA[key].name,
+            icon: SOCIAL_MEDIA[key].icon,
+            url: value,
+
+        })
+    })
+    return snsList;
+}
+
+export function getProfile() {
+    return { ...profile };
+}
---
+++ src/config/ui.ts
@@ -0,0 +1,12 @@
+import { Linkedin, IdCard } from 'lucide-react';
+
+export const SOCIAL_MEDIA: Record<string, { name: string; icon: any }> = {
+    linkedin: {
+        name: 'Linkedin',
+        icon: Linkedin,
+    },
+    orcid: {
+        name: 'ORCID',
+        icon: IdCard,
+    },
+};
